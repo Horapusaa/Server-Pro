c0d11d6bc04c437580129eadabe2065251a73063
Hora Pusa <73831309+Horapusaa@users.noreply.github.com>
2022-02-10 10:18:30 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,6 @@
 git clone https://github.com/coder/code-server
 cd code-server
-
-bind-addr: 0.0.0.0:8080
-auth: password
-password: horapusa
-cert: false
-
-
-
+bash install.sh
 cat << EOF > /usr/local/etc/xray/config0.json
 {
 bind-addr: 0.0.0.0:8080
